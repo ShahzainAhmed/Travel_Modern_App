8333ad2902380d2f3141e9db4750226d97fb1852
Shahzain Ahmed <shahzainahmed57@gmail.com>
2024-07-31 12:32:50 +0500

    create build & release

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:travel_app/view/home/home_screen.dart';
 import 'package:travel_app/view/splash/splash_screen.dart';
 
 void main() {
